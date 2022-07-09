f5268102b3afd16b1fc47c2aa52ccdd5a68e2c25
DevonPeroutky <devonperoutky@gmail.com>
2022-07-09 18:37:07 -0400

    Performant (but not blended) lenticular painting

--- paintings/lenticular/main.py
+++ paintings/lenticular/main.py
@@ -12,7 +12,16 @@ fifo = None
 # Variables set by external video_face_tracker
 distance_from_camera = 0
 angle_from_camera = 0
+
+# TODO: Change x_pos received to be a percentage?!?!?!
+# The most up-to-date x_pox (being updated very fast)
+curr_x_pos = 0
+
+# Lock the x_pos in.
 x_pos = 0
+
+# The previously "locked-in" x_pos
+last_x_pos = 0
 distance_from_center = 0
 
 # Constants
@@ -21,39 +30,56 @@ render_height = 1586
 
 # Variables
 lenticular_img = None
-coordinator = LenticularCoordinator(render_width, render_height, ["../../images/devon_young.jpg", "../../images/devon_old.jpg"])
+coordinator = None
 
 def setup():
-    global fifo, lenticular_img
+    global fifo, lenticular_img, coordinator
     frameRate(24)
     thread("grab_emotional_parameters")
     size(render_width, render_height)
+    coordinator = LenticularCoordinator(render_width, render_height, ["../../images/devon_young.jpg", "../../images/devon_old.jpg"])
     lenticular_img = createImage(render_width, render_height, RGB)
     lenticular_img.loadPixels()
+    print("Created image with %s pixels" % len(lenticular_img.pixels))
+
+    # INITIAL RENDER
+    new_pixels = coordinator.calc_pixel_diffs(x_pos=render_width, last_x_pos=0)
+    coordinator.update_img_pixels(img=lenticular_img, new_pixels=new_pixels, x_pos=x_pos)
+    lenticular_img.updatePixels()
+    image(lenticular_img, 0, 0)
 
 def draw():
-    global fifo, emotion, x_pos, lenticular_img
-    background(255, 255, 255)
+    global fifo, emotion, x_pos, last_x_pos, curr_x_pos, lenticular_img, coordinator
+    last_x_pos = x_pos
+    x_pos = curr_x_pos
     println(frameRate);
 
     # Determine Pixel Array
-    rendered_pixel_array = coordinator.render_image_pixels(x_pos=x_pos)
+    # rendered_pixel_array = coordinator.render_image_pixels(x_pos=x_pos)
+
+    # Determine which pixels need to be reevaluated
+    new_pixels = coordinator.calc_pixel_diffs(x_pos=x_pos, last_x_pos=last_x_pos)
+
+    # Update the image's pixels
+    coordinator.update_img_pixels(img=lenticular_img, new_pixels=new_pixels, x_pos=x_pos)
     
     # Update image on the screen
-    # for i in range(len(lenticular_img.pixels)):
-    #     lenticular_img.pixels[i] = rendered_pixel_array[i]
-    # lenticular_img.updatePixels()
+    lenticular_img.updatePixels()
 
     # Render Image
-    # image(lenticular_img, 0, 0)
+    image(lenticular_img, 0, 0)
 
+def set_x_pos(received_x_pos):
+    global render_width, curr_x_pos
+    """
+    OpenCV is right-to-left because its from perspective of the camera. Processing is left to right
+    """
+    curr_x_pos = render_width - received_x_pos
 
-def draw_new_image():
-    pass
 
 
 def grab_emotional_parameters():
-    global distance_from_camera, angle_from_camera, x_pos
+    global distance_from_camera, angle_from_camera, curr_x_pos
 
     PIPE_PATH = '/tmp/test'
     while True:
@@ -68,7 +94,7 @@ def grab_emotional_parameters():
                         location_data = json.loads(line)
                         distance_from_camera = location_data.get('distance_in_inches', '50')
                         angle_from_camera = location_data.get('angle_from_camera', '0')
-                        x_pos = location_data.get('x_pos', '0')
+                        set_x_pos(int(location_data.get('x_pos', '0')))
                         distance_from_center = location_data.get('distance_from_center', '0')
                     except ValueError as e:
                         print("Unable to parse Json")
